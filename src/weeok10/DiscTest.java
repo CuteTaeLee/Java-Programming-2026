a3c4af70a069a79254c60a98a6b126317bea96a0
seoil <seoil@BY307-G-24>
2026-05-06 14:10:25 +0900

    1교시 랜덤활용하여 주사위 객체,클래스,함수

---
+++ src/weeok10/DiscTest.java
@@ -0,0 +1,22 @@
+package weeok10;
+
+class Dice {
+
+	int face;
+
+	public int roll() {
+		face = (int) (Math.random() * 6) + 1;
+		return face;
+	}
+}
+
+public class DiscTest {
+
+	public static void main(String[] args) {
+
+		Dice disc = new Dice();
+		System.out.println("주사위 숫자:" + disc.roll());
+
+	}
+
+}
